02b09bd6f64e2b8eb0897708a4b86e7783f73c73
Timokrut <potapov.tim14@gmail.com>
2025-09-29 22:47:13 +0300

    modifications

--- 1/main.py
+++ 1/main.py
@@ -1,6 +1,7 @@
 import math
 import numpy as np
 import matplotlib.pyplot as plt
+import pandas as pd
 
 def count_T(V_mod: int) -> float:
     return 1 / V_mod
@@ -32,13 +33,18 @@ def calculate_signal(s_i: tuple, T: float, f_0: int, t: np.ndarray) -> np.ndarra
 def calculate_energy_numerical(signal: np.ndarray, dt: float):
     return np.sum(signal ** 2) * dt
 
+def calculate_energy_analytical(s_i: tuple[float, float]) -> float: 
+    si_1, si_2 = s_i 
+    return si_1**2 + si_2**2 
+
+
 if __name__ == '__main__':
     f_0 = 1200  # Hz
     V_mod = 600  # Baud
     V_inf = 2400  # bit/sec
 
     A = 1.0  # Амплитуда
-    Ns = 12  # Количество samples на период несущей
+    Ns = 128  # Количество samples на период несущей
 
     # Вычисление основных параметров
     T = count_T(V_mod)
@@ -65,6 +71,7 @@ if __name__ == '__main__':
 
     print("\nЭНЕРГИИ СИГНАЛОВ (численный расчет):")
     numerical_energies = []
+    analytical_energies = []
     signals = []
     for i, pair in enumerate(i_pairs):
         s_i = count_si_values(pair, q, A)
@@ -72,7 +79,22 @@ if __name__ == '__main__':
         signals.append(signal)
         energy_numerical = calculate_energy_numerical(signal, dt)
         numerical_energies.append(energy_numerical)
+        energy_analytical = calculate_energy_analytical(s_i) 
+        analytical_energies.append(energy_analytical)
         print(f"Сигнал {i} ({pair}): E = {energy_numerical:.3f}")
+
+    # === ТАБЛИЦА СИГНАЛОВ === 
+    table = [] 
+    for i, pair in enumerate(i_pairs): 
+        s_i = count_si_values(pair, q, A) 
+        s_expr = f"{s_i[0]:.3f}*√(2/T)*cos(2π*{f_0}*t) + {s_i[1]:.3f}*√(2/T)*sin(2π*{f_0}*t)" 
+        table.append([i, pair[0], pair[1], s_i[0], s_i[1], numerical_energies[i], analytical_energies[i], s_expr]) 
+ 
+    columns = ["i", "i1", "i2", "s_i1", "s_i2", "E_числ", "E_теор", "Формула"] 
+    df = pd.DataFrame(table, columns=columns) 
+    print("\nПОЛНАЯ ТАБЛИЦА СИГНАЛОВ:") 
+    print(df.to_string(index=False)) 
+ 
 
     fig, axes = plt.subplots(4, 4, figsize=(15, 10))
     fig.suptitle(f'Сигналы КАМ (q={q}, V_mod={V_mod} Бод, V_inf={V_inf} бит/сек)', fontsize=16)
@@ -88,3 +110,15 @@ if __name__ == '__main__':
 
     plt.tight_layout()
     plt.show() 
+
+    # график энергий номер сигнала по оси икс, эенергии по y 
+    plt.figure(figsize=(10, 5)) 
+    plt.plot(numerical_energies, 'o-', label='E (дискретно)', markersize=4, linewidth=1) 
+    plt.plot(analytical_energies, '--', label='E (теоретически)', linewidth=2)
+    plt.title(f"Сравнение энергий всех {q} сигналов") 
+    plt.xlabel("Номер сигнала i") 
+    plt.ylabel("Энергия") 
+    plt.grid(True, alpha=0.3) 
+    plt.legend() 
+    plt.tight_layout() 
+    plt.show()
